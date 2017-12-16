0174cc4f26491cc7d89ceaa75e9d9e37d2fb4a24
Gary Bortosky <garybort@gmail.com>
2017-12-16 02:25:06 -0600

    decent diffs

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -11,26 +11,75 @@ popup.start(document.body, 'Suggested Edit');
 
 import '../styles/base.scss';
 
+function makeDiffDiv(
+  start: string,
+  strike: string,
+  ins: string,
+  end: string
+): HTMLDivElement {
+  const div = document.createElement('div');
+  div.style.margin = '10px';
+  div.appendChild(
+    makeSpan({ backgroundColor: 'white' }, `...${start.trimLeft()}`)
+  );
+  div.appendChild(
+    makeSpan({ backgroundColor: 'red', textDecoration: 'line-through' }, strike)
+  );
+  div.appendChild(makeSpan({ backgroundColor: 'green' }, ins));
+  div.appendChild(
+    makeSpan({ backgroundColor: 'white' }, `${end.trimRight()}...`)
+  );
+  return div;
+}
+
 async function processSuggestion(
   msg: messages.MakeSuggestionMessage
 ): Promise<boolean> {
   const subject = await domutils.getSubjectInfo(msg.elementPath, msg.original);
   const { element, textNode } = subject;
+  const bishop = document.querySelector('#bishop');
   console.log(subject);
   if (element) {
     element.style.border = 'thin solid silver';
     if (textNode && textNode.textContent) {
       const text = textNode.textContent;
-      const gh = [
-        text.slice(0, msg.selectionStart),
-        msg.suggestedText,
-        text.slice(msg.selectionStart + msg.selectionLength),
-      ];
-      textNode.textContent = gh.join('<=>');
-      // console.log(gh.join('<=>'));
+      const start = text.slice(0, msg.selectionStart);
+      const strike = msg.original.slice(
+        msg.selectionStart,
+        msg.selectionStart + msg.selectionLength
+      );
+      const ins = msg.suggestedText;
+      const end = text.slice(msg.selectionStart + msg.selectionLength);
+
+      if (bishop) {
+        const cntnr = document.createElement('div');
+        cntnr.style.margin = '10px';
+        cntnr.style.padding = '10px';
+        cntnr.style.backgroundColor = '#cccccc';
+
+        const ls = start.split('\n');
+
+        cntnr.appendChild(makeDiffDiv(ls[ls.length - 1], strike, ins, end));
+        bishop.appendChild(cntnr);
+      }
     }
   }
   return Promise.resolve(true);
+}
+
+function makeSpan(
+  style: Partial<CSSStyleDeclaration>,
+  innerText: string
+): HTMLSpanElement {
+  const el = document.createElement('span');
+  el.innerText = innerText;
+
+  Object.assign(el.style, style);
+
+  // el.style=style;
+  // el.style.backgroundColor = backgroundColor;
+  // el.style.textDecoration = 'line-through';
+  return el;
 }
 
 async function startSuggestion(
@@ -101,27 +150,27 @@ chrome.runtime.onMessage.addListener(
 //   type: 'MAKE_SUGGESTION',
 // };
 
-// const tester: messages.MakeSuggestionMessage = {
-//   elementPath: [
-//     ['BODY', 1],
-//     ['DIV', 0],
-//     ['DIV', 1],
-//     ['DIV', 0],
-//     ['DIV', 0],
-//     ['ARTICLE', 0],
-//     ['DIV', 1],
-//     ['P', 3],
-//   ],
-//   href:
-//     'https://blog.jcoglan.com/2017/03/22/myers-diff-in-linear-space-theory/',
-//   original:
-//     ' and gone back to make progress on another project, I stumbled\ninto a case where Git produced a confusing diff for a file I’d just changed, and\nI had to know why. Here’s the portion of code I had been working on. It’s a\ncouple of C functions that copy bytes from one buffer to another, checking the\nsizes of the requested regions to make sure they’re within the buffer. (This is\nnot literally the code I was working on; I’ve removed a few things to make the\nexample smaller.)',
-//   selectionLength: 7,
-//   selectionStart: 174,
-//   suggestedText: 'part',
-//   textNodeIndex: 2,
-//   type: 'MAKE_SUGGESTION',
-// };
+const jcoglan: messages.MakeSuggestionMessage = {
+  elementPath: [
+    ['BODY', 1],
+    ['DIV', 0],
+    ['DIV', 1],
+    ['DIV', 0],
+    ['DIV', 0],
+    ['ARTICLE', 0],
+    ['DIV', 1],
+    ['P', 3],
+  ],
+  href:
+    'https://blog.jcoglan.com/2017/03/22/myers-diff-in-linear-space-theory/',
+  original:
+    ' and gone back to make progress on another project, I stumbled\ninto a case where Git produced a confusing diff for a file I’d just changed, and\nI had to know why. Here’s the portion of code I had been working on. It’s a\ncouple of C functions that copy bytes from one buffer to another, checking the\nsizes of the requested regions to make sure they’re within the buffer. (This is\nnot literally the code I was working on; I’ve removed a few things to make the\nexample smaller.)',
+  selectionLength: 7,
+  selectionStart: 174,
+  suggestedText: 'part',
+  textNodeIndex: 2,
+  type: 'MAKE_SUGGESTION',
+};
 
 const testers: messages.MakeSuggestionMessage[] = [
   {
@@ -167,7 +216,12 @@ const testers: messages.MakeSuggestionMessage[] = [
   },
 ];
 
+testers.push(jcoglan);
+
 window.setTimeout(() => {
+  const aa = document.createElement('div');
+  aa.id = 'bishop';
+  document.body.appendChild(aa);
   for (const tester of testers) {
     if (tester.href === window.location.href) {
       processSuggestion(tester);
