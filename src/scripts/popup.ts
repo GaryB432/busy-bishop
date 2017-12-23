e55059319bc8ecd26297b401b5cddf589db34f02
Gary Bortosky <garybort@gmail.com>
2017-12-22 20:27:22 -0600

    popup structure

--- src/scripts/popup.ts
+++ src/scripts/popup.ts
@@ -1,62 +1,77 @@
 import '../styles/popup.scss';
 import * as data from './lib/data';
+import * as messages from './lib/messages';
 import * as utilities from './lib/utilities';
 
-const elem = document.querySelector('#suggestions');
+// const elem = document.querySelector('#suggestions');
 
+const schemeRegex: RegExp = new RegExp('https*://', 'ig');
 const ltrim = (str: string) => str.replace(/^\s+/, '');
 const rtrim = (str: string) => str.replace(/\s+$/, '');
+const schemeTrim = (str: string) => str.replace(schemeRegex, '');
 
-export function createDiffElement(
+function asdf(templateId: string): DocumentFragment {
+  const template$ = document.querySelector(
+    templateId
+  ) as HTMLTemplateElement;
+  if (!template$) {
+    throw new Error('sorry');
+  }
+  return document.importNode(template$.content, true);
+}
+
+function createDiffElement(
   start: string,
   strike: string,
   ins: string,
   end: string
 ): DocumentFragment {
-  const template = document.querySelector(
-    '#diffTemplate'
-  ) as HTMLTemplateElement;
-  if (!template) {
-    throw new Error('sorry');
-  }
-  const div = document.importNode(template.content, true);
-  const spans: NodeListOf<HTMLSpanElement> = div.querySelectorAll(
+  const div$ = asdf('#diffTemplate');
+  const spans: NodeListOf<HTMLSpanElement> = div$.querySelectorAll(
     'div.diff > span'
   );
   spans[0].innerHTML = `&hellip;${ltrim(start)}`;
   spans[1].innerHTML = strike;
   spans[2].innerHTML = ins;
   spans[3].innerHTML = `${rtrim(end)}&hellip;`;
-  return div;
+  return div$;
+}
+function addSuggestionElement(suggestion: messages.MakeSuggestionMessage, sugs$: HTMLDivElement) {
+  const flx = utilities.narrowSelectionContext(suggestion.context, suggestion.selectedText);
+  if (!!flx) {
+    const { line, index } = flx;
+    const selectionLength = suggestion.selectedText.length;
+    const start = line.slice(0, index);
+    const strike = line.slice(index, index + selectionLength);
+    const ins = suggestion.suggestedText!;
+    const end = line.slice(index + selectionLength);
+    sugs$.appendChild(createDiffElement(start, strike, ins, end));
+  }
 }
 
 async function processUrl(url?: string): Promise<boolean> {
-  const sugs = document.createElement('div');
+  // const sugs$ = document.createElement('div');
+  const sugs$ = document.querySelector<HTMLDivElement>('#suggestions')!;
   if (!!url) {
-    const testers = await data.getForHref(url);
-    for (const tester of testers) {
-      const flx = utilities.narrowSelectionContext(
-        tester.context,
-        tester.selectedText
-      );
-      console.log(flx, tester.context, tester.selectedText);
-
-      if (!!flx) {
-        const { line, index } = flx;
-        const selectionLength = tester.selectedText.length;
-        const start = line.slice(0, index);
-        const strike = line.slice(index, index + selectionLength);
-        const ins = tester.suggestedText!;
-        const end = line.slice(index + selectionLength);
-        sugs.appendChild(createDiffElement(start, strike, ins, end));
-      }
+    const url$ = document.querySelector('#url');
+    url$!.textContent = schemeTrim(url);
+    const suggestions = await data.getSuggestionsForHref(url);
+    for (const s of suggestions) {
+      addSuggestionElement(s, sugs$);
     }
   }
-  elem!.appendChild(sugs);
+  // elem!.appendChild(sugs$);
   return true;
 }
 
-chrome.tabs.query(
-  { active: true, currentWindow: true },
-  (tabs: chrome.tabs.Tab[]) => processUrl(tabs[0].url)
-);
+if (chrome.tabs) {
+  chrome.tabs.query(
+    { active: true, currentWindow: true },
+    (tabs: chrome.tabs.Tab[]) => processUrl(tabs[0].url)
+  );
+} else {
+  processUrl(
+    'https://blog.jcoglan.com/2017/03/22/myers-diff-in-linear-space-theory/'
+  );
+}
+
