60f7a340bf3d33d5943661f607da7502a9e969ce
Gary Bortosky <garybort@gmail.com>
2017-12-30 16:16:12 -0600

    add get suggestion function

---
+++ hosted-functions/get-suggestions/index.ts
@@ -0,0 +1,90 @@
+import { DocumentClient } from 'documentdb';
+import { SuggestionDao } from './imported/common/lib/data/suggestion-dao';
+import { SuggestionDocument } from './imported/common/lib/models';
+import { environment } from './imported/common/lib/imported/environments/environment';
+
+interface Environment {
+  functionKeys: {
+    writeSuggestion: string
+  };
+  mainDb: {
+    connection: string;
+    host: string;
+    masterKey: string
+  };
+}
+
+const config: Environment = environment;
+const { host, masterKey } = config.mainDb;
+const client = new DocumentClient(host, { masterKey: masterKey });
+
+export type Doc = SuggestionDocument;
+
+export interface Context {
+  log(...message: any[]): void;
+  done(): void;
+  bindings: never;
+  res: Response;
+}
+
+export interface Request {
+  method: 'GET' | 'POST';
+  query: { href?: string };
+}
+
+type ResponseBody = Doc[];
+
+export interface Response {
+  status: number;
+  body: ResponseBody | string;
+}
+
+const dao = new SuggestionDao(client, 'Main', 'Suggestions');
+
+export default function (context: Context, req: Request) {
+  const response: Response = {
+    status: 400,
+    body: 'Invalid Request',
+  };
+  function getDocuments(href: string): Promise<SuggestionDocument[]> {
+    context.log(href);
+    return new Promise<SuggestionDocument[]>((resolve, reject) => {
+      dao.init((err, coll) => {
+        if (coll) {
+          dao.getByHref(
+            href,
+            (err2, suggestions) => {
+              if (err2) {
+                context.log(err2);
+                reject(err2);
+              } else if (suggestions) {
+                resolve(suggestions);
+              }
+            }
+          );
+        } else {
+          context.log(err!);
+        }
+      });
+    });
+  }
+
+  function finishUp() {
+    context.res = response;
+    context.done();
+  }
+
+  if (req.method === 'GET') {
+    if (req.query.href) {
+      response.status = 200;
+      getDocuments(req.query.href).then((documents: SuggestionDocument[]) => {
+        response.body = documents;
+        finishUp();
+      });
+    } else {
+      finishUp();
+    }
+  } else {
+    finishUp();
+  }
+}
--- hosted-functions/write-suggestion/index.ts
+++ hosted-functions/write-suggestion/index.ts
@@ -1,11 +1,19 @@
 // https://docs.microsoft.com/en-us/azure/azure-functions/functions-reference-node
 
-export interface Doc {
+
+interface SuggestionDocumentV1 {
   id: string;
-  name: string;
-  employeeId: number;
-  address: string;
+  elementPath: string;
+  context: string;
+  textNodeIndex: number;
+  selectedText: string;
+  selectionStart: number;
+  suggestedText?: string;
+  href: string;
+  createdAt: number;
 }
+
+export type Doc = SuggestionDocumentV1;
 
 export interface Bindings {
   suggestionDocument: Doc;
@@ -24,44 +32,26 @@ export interface Request {
   query: any;
 }
 
+type ResponseBody = Doc;
+
 export interface Response {
   status: number;
-  body: any;
+  body: ResponseBody | string;
 }
 
-
-// module.exports = function (context, req) {
-//   var statusCode = 400;
-//   var responseBody = "Invalid request object";
-
-//   if (typeof req.body != 'undefined' && typeof req.body == 'object') {
-//       statusCode = 201;
-//       context.bindings.outTable = req.body;
-//       responseBody = "Table Storage Created";
-//   }
-
-//   context.res = {
-//       status: statusCode,
-//       body: responseBody
-//   };
-
-//   context.done();
-// };
-
-
 export default (context: Context, req: Request) => {
-  let statusCode = 400;
-  let responseBody = '?';
+  const res: Response = {
+    status: 400,
+    body: 'Invalid Request'
+  }
   if (req.method === "POST") {
     if (req.body) {
-      statusCode = 201;
-      context.bindings.suggestionDocument = req.body;
-      responseBody = 'Document created';
+      res.status = 201;
+      const suggestion: Doc = req.body;
+      context.bindings.suggestionDocument = suggestion;
+      res.body = JSON.stringify(suggestion);
     }
   }
-  context.res = {
-    status: statusCode,
-    body: responseBody
-  }
+  context.res = res;
   context.done();
 };
