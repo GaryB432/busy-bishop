f526a8116dd339332bb6cb42a2d9e2727800f4a0
Gary Bortosky <garybort@gmail.com>
2017-12-30 16:09:51 -0600

    add some admin scripts

---
+++ scripts/Build-All.ps1
@@ -0,0 +1,16 @@
+﻿
+Param([bool]$install = $False, [bool]$build = $False, [string]$bar = "bar")
+
+$projs = @("common", "hosted-functions", "app", "extension")
+
+ForEach ($proj in $projs) {
+	"$proj = " + $proj.length
+	Push-Location $proj
+	if ($install) {
+		npm install 
+	}
+	if ($build) {
+		npm run build
+	}
+	Pop-Location
+}
---
+++ scripts/Share-Common.ps1
@@ -0,0 +1,17 @@
+﻿Remove-Item .\_for-import -Recurse -ErrorAction SilentlyContinue
+
+Copy-Item .\secret\ -Destination .\_for-import\ -Force -Recurse
+Copy-Item .\common\src\models.ts .\_for-import\models.ts
+
+Remove-Item .\common\src\imported -Recurse -Force -ErrorAction SilentlyContinue
+Copy-Item .\_for-import -Destination .\common\src\imported -Recurse
+
+Remove-Item .\app\src\imported -Recurse -Force -ErrorAction SilentlyContinue
+Copy-Item .\_for-import -Destination .\app\src\imported -Recurse 
+
+
+Push-Location common
+# npm run build
+Pop-Location
+
+Copy-Item .\common\lib -Destination .\hosted-functions\get-suggestions\imported\common -Force -Recurse
